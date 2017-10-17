19c1301b8037466cf3ca3c9e73f79b7a88252cad
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2017-10-17 12:46:10 +0100

    Run properly the API tests for both the app and service clients

--- cloudfoundry/usage_events_api_test.go
+++ cloudfoundry/usage_events_api_test.go
@@ -3,6 +3,7 @@ package cloudfoundry_test
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -17,25 +18,28 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("The Usage Events Handler", func() {
-	var (
-		now                  time.Time
-		mockCtrl             *gomock.Controller
-		logger               lager.Logger
-		mockClient           *mocks.MockClient
-		usageEvents          UsageEventsAPI
-		emptyUsageList       string
-		usageListWithRecords string
-	)
+type clientFactory func(client Client, logger lager.Logger) UsageEventsAPI
 
-	BeforeEach(func() {
-		now = time.Now()
-		mockCtrl = gomock.NewController(GinkgoT())
-		mockClient = mocks.NewMockClient(mockCtrl)
-		logger = lager.NewLogger("test")
-		usageEvents = NewAppUsageEventsAPI(mockClient, logger)
+var usageEventTests = func(eventType string, clientFactory clientFactory) func() {
+	return func() {
+		var (
+			now                  time.Time
+			mockCtrl             *gomock.Controller
+			logger               lager.Logger
+			mockClient           *mocks.MockClient
+			usageEvents          UsageEventsAPI
+			emptyUsageList       string
+			usageListWithRecords string
+		)
 
-		emptyUsageList = `{
+		BeforeEach(func() {
+			now = time.Now()
+			mockCtrl = gomock.NewController(GinkgoT())
+			mockClient = mocks.NewMockClient(mockCtrl)
+			logger = lager.NewLogger("test")
+			usageEvents = clientFactory(mockClient, logger)
+
+			emptyUsageList = `{
   "total_results": 0,
   "total_pages": 1,
   "prev_url": null,
@@ -43,7 +47,7 @@ var _ = Describe("The Usage Events Handler", func() {
   "resources": []
 }`
 
-		usageListWithRecords = `{
+			usageListWithRecords = `{
   "total_results": 3,
   "total_pages": 1,
   "prev_url": null,
@@ -52,7 +56,7 @@ var _ = Describe("The Usage Events Handler", func() {
     {
       "metadata": {
         "guid": "a000",
-        "url": "/v2/app_usage_events/a000",
+        "url": "/v2/` + eventType + `_usage_events/a000",
         "created_at": "` + now.Add(-2*time.Minute).Format("2006-01-02T15:04:05Z07:00") + `"
       },
       "entity": {
@@ -62,7 +66,7 @@ var _ = Describe("The Usage Events Handler", func() {
     {
       "metadata": {
         "guid": "b000",
-        "url": "/v2/app_usage_events/b000",
+        "url": "/v2/` + eventType + `_usage_events/b000",
         "created_at": "` + now.Add(-1*time.Minute).Format("2006-01-02T15:04:05Z07:00") + `"
       },
       "entity": {
@@ -72,7 +76,7 @@ var _ = Describe("The Usage Events Handler", func() {
     {
       "metadata": {
         "guid": "c000",
-        "url": "/v2/app_usage_events/c000",
+        "url": "/v2/` + eventType + `_usage_events/c000",
         "created_at": "` + now.Format("2006-01-02T15:04:05Z07:00") + `"
       },
       "entity": {
@@ -81,203 +85,193 @@ var _ = Describe("The Usage Events Handler", func() {
     }
   ]
 }`
-	})
-
-	AfterEach(func() {
-		mockCtrl.Finish()
-	})
-
-	Describe("Usage events API client", func() {
-
-		Context("When created for the apps", func() {
-			It("should use the app API point", func() {
-				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
-				}
-				mockClient.EXPECT().Get("/v2/app_usage_events?results-per-page=3").Return(resp, nil)
-				client := NewAppUsageEventsAPI(mockClient, logger)
-				Expect(client.Type()).To(BeEquivalentTo("app"))
-				client.Get(GUIDNil, 3, 0)
-			})
 		})
 
-		Context("When created for the service", func() {
-			It("should use the service API point", func() {
-				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
-				}
-				mockClient.EXPECT().Get("/v2/service_usage_events?results-per-page=3").Return(resp, nil)
-				client := NewServiceUsageEventsAPI(mockClient, logger)
-				Expect(client.Type()).To(BeEquivalentTo("service"))
-				client.Get(GUIDNil, 3, 0)
-			})
+		AfterEach(func() {
+			mockCtrl.Finish()
 		})
 
-		Context("When the API is called", func() {
+		Describe("Usage events API client", func() {
 
-			It("should set the results-per-page parameter", func() {
-				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
-				}
-				mockClient.EXPECT().Get("/v2/app_usage_events?results-per-page=3").Return(resp, nil)
-				usageEvents.Get(GUIDNil, 3, 0)
+			Context("When created", func() {
+				It("should have the right type", func() {
+					client := clientFactory(mockClient, logger)
+					Expect(client.Type()).To(BeEquivalentTo(eventType))
+				})
 			})
 
-			It("should set the after_guid parameter if set", func() {
-				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
-				}
-				mockClient.EXPECT().Get("/v2/app_usage_events?results-per-page=3&after_guid=abcd").Return(resp, nil)
-				usageEvents.Get("abcd", 3, 0)
+			Context("When the API is called", func() {
+
+				It("should create the proper API url", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
+					}
+					mockClient.EXPECT().Get(fmt.Sprintf("/v2/%s_usage_events?results-per-page=3", eventType)).Return(resp, nil)
+					usageEvents.Get(GUIDNil, 3, 0)
+				})
+
+				It("should set the after_guid parameter if set", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
+					}
+					mockClient.EXPECT().Get(fmt.Sprintf("/v2/%s_usage_events?results-per-page=3&after_guid=abcd", eventType)).Return(resp, nil)
+					usageEvents.Get("abcd", 3, 0)
+				})
+
+				It("should handle empty results", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
+					}
+					mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
+					events, err := usageEvents.Get(GUIDNil, 3, 0)
+					Expect(err).To(BeNil(), "there should be no error")
+					Expect(events).To(BeEquivalentTo(&UsageEventList{Resources: []UsageEvent{}}), "result should be empty list")
+				})
+
+				It("should parse result list correctly", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					}
+					mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
+					events, err := usageEvents.Get(GUIDNil, 10, 0)
+					Expect(err).To(BeNil(), "there should be no error")
+
+					expected := &UsageEventList{
+						Resources: []UsageEvent{
+							{
+								MetaData: MetaData{
+									GUID:      "a000",
+									CreatedAt: now.Add(-2 * time.Minute).Truncate(time.Second),
+								},
+								EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo1\"\n      }")),
+							},
+							{
+								MetaData: MetaData{
+									GUID:      "b000",
+									CreatedAt: now.Add(-1 * time.Minute).Truncate(time.Second),
+								},
+								EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo2\"\n      }")),
+							},
+							{
+								MetaData: MetaData{
+									GUID:      "c000",
+									CreatedAt: now.Truncate(time.Second),
+								},
+								EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo3\"\n      }")),
+							},
+						},
+					}
+
+					Expect(events).To(BeEquivalentTo(expected))
+				})
+
+				It("should return results only with minimum age", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					}
+					mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
+					events, err := usageEvents.Get(GUIDNil, 10, 2*time.Minute)
+					Expect(err).To(BeNil(), "there should be no error")
+
+					expected := &UsageEventList{
+						Resources: []UsageEvent{
+							{
+								MetaData: MetaData{
+									GUID:      "a000",
+									CreatedAt: now.Add(-2 * time.Minute).Truncate(time.Second),
+								},
+								EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo1\"\n      }")),
+							},
+						},
+					}
+
+					Expect(events).To(BeEquivalentTo(expected))
+				})
+
+				It("should return empty list if every record is newer than minimum age", func() {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					}
+					mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
+					events, err := usageEvents.Get(GUIDNil, 10, 10*time.Minute)
+					Expect(err).To(BeNil(), "there should be no error")
+					Expect(events).To(BeEquivalentTo(&UsageEventList{Resources: []UsageEvent{}}))
+				})
+
 			})
 
-			It("should handle empty results", func() {
-				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(emptyUsageList)),
-				}
-				mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-				events, err := usageEvents.Get(GUIDNil, 3, 0)
-				Expect(err).To(BeNil(), "there should be no error")
-				Expect(events).To(BeEquivalentTo(&UsageEventList{Resources: []UsageEvent{}}), "result should be empty list")
+		})
+
+		Context("When the API request fails", func() {
+
+			It("should handle client error", func() {
+				mockClient.EXPECT().Get(gomock.Any()).Return(nil, errors.New("some error"))
+				events, err := usageEvents.Get(GUIDNil, 10, 0)
+				Expect(err.Error()).ToNot(BeNil())
+				Expect(err.Error()).To(BeEquivalentTo(fmt.Sprintf("error fetching /v2/%s_usage_events?results-per-page=10: some error", eventType)))
+				Expect(events).To(BeNil())
 			})
 
-			It("should parse result list correctly", func() {
+			It("should handle non-200 http status code", func() {
 				resp := &http.Response{
-					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					StatusCode: 500,
+					Body:       ioutil.NopCloser(strings.NewReader("some error")),
 				}
 				mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
 				events, err := usageEvents.Get(GUIDNil, 10, 0)
-				Expect(err).To(BeNil(), "there should be no error")
-
-				expected := &UsageEventList{
-					Resources: []UsageEvent{
-						{
-							MetaData: MetaData{
-								GUID:      "a000",
-								CreatedAt: now.Add(-2 * time.Minute).Truncate(time.Second),
-							},
-							EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo1\"\n      }")),
-						},
-						{
-							MetaData: MetaData{
-								GUID:      "b000",
-								CreatedAt: now.Add(-1 * time.Minute).Truncate(time.Second),
-							},
-							EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo2\"\n      }")),
-						},
-						{
-							MetaData: MetaData{
-								GUID:      "c000",
-								CreatedAt: now.Truncate(time.Second),
-							},
-							EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo3\"\n      }")),
-						},
-					},
-				}
-
-				Expect(events).To(BeEquivalentTo(expected))
+				Expect(err.Error()).ToNot(BeNil())
+				Expect(err.Error()).To(BeEquivalentTo(fmt.Sprintf("/v2/%s_usage_events?results-per-page=10 request failed: 500 some error", eventType)))
+				Expect(events).To(BeNil())
 			})
 
-			It("should return results only with minimum age", func() {
+			It("should handle non valid JSON response", func() {
 				resp := &http.Response{
 					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					Body:       ioutil.NopCloser(strings.NewReader("non json")),
 				}
 				mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-				events, err := usageEvents.Get(GUIDNil, 10, 2*time.Minute)
-				Expect(err).To(BeNil(), "there should be no error")
-
-				expected := &UsageEventList{
-					Resources: []UsageEvent{
-						{
-							MetaData: MetaData{
-								GUID:      "a000",
-								CreatedAt: now.Add(-2 * time.Minute).Truncate(time.Second),
-							},
-							EntityRaw: json.RawMessage([]byte("{\n        \"field\": \"foo1\"\n      }")),
-						},
-					},
-				}
-
-				Expect(events).To(BeEquivalentTo(expected))
+				events, err := usageEvents.Get(GUIDNil, 10, 0)
+				Expect(err.Error()).ToNot(BeNil())
+				Expect(events).To(BeNil())
 			})
 
-			It("should return empty list if every record is newer than minimum age", func() {
+			It("should handle IO error", func() {
+				bodyMock := mocks.NewMockReadCloser(mockCtrl)
+				bodyMock.EXPECT().Read(gomock.Any()).Return(0, errors.New("some error"))
+				bodyMock.EXPECT().Close()
 				resp := &http.Response{
 					StatusCode: 200,
-					Body:       ioutil.NopCloser(strings.NewReader(usageListWithRecords)),
+					Body:       bodyMock,
 				}
 				mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-				events, err := usageEvents.Get(GUIDNil, 10, 10*time.Minute)
-				Expect(err).To(BeNil(), "there should be no error")
-				Expect(events).To(BeEquivalentTo(&UsageEventList{Resources: []UsageEvent{}}))
+				events, err := usageEvents.Get(GUIDNil, 10, 0)
+				Expect(err.Error()).ToNot(BeNil())
+				Expect(events).To(BeNil())
 			})
 
 		})
 
-	})
+		Context("When the method is not used correctly", func() {
 
-	Context("When the API request fails", func() {
+			It("should panic if GUID is empty", func() {
+				Expect(func() { usageEvents.Get("", 10, 0) }).To(Panic())
+			})
 
-		It("should handle client error", func() {
-			mockClient.EXPECT().Get(gomock.Any()).Return(nil, errors.New("some error"))
-			events, err := usageEvents.Get(GUIDNil, 10, 0)
-			Expect(err.Error()).ToNot(BeNil())
-			Expect(err.Error()).To(BeEquivalentTo("error fetching /v2/app_usage_events?results-per-page=10: some error"))
-			Expect(events).To(BeNil())
 		})
 
-		It("should handle non-200 http status code", func() {
-			resp := &http.Response{
-				StatusCode: 500,
-				Body:       ioutil.NopCloser(strings.NewReader("some error")),
-			}
-			mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-			events, err := usageEvents.Get(GUIDNil, 10, 0)
-			Expect(err.Error()).ToNot(BeNil())
-			Expect(err.Error()).To(BeEquivalentTo("/v2/app_usage_events?results-per-page=10 request failed: 500 some error"))
-			Expect(events).To(BeNil())
-		})
+	}
+}
 
-		It("should handle non valid JSON response", func() {
-			resp := &http.Response{
-				StatusCode: 200,
-				Body:       ioutil.NopCloser(strings.NewReader("non json")),
-			}
-			mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-			events, err := usageEvents.Get(GUIDNil, 10, 0)
-			Expect(err.Error()).ToNot(BeNil())
-			Expect(events).To(BeNil())
-		})
+var _ = Describe("The App Usage Events Handler", usageEventTests("app", func(client Client, logger lager.Logger) UsageEventsAPI {
+	return NewAppUsageEventsAPI(client, logger)
+}))
 
-		It("should handle IO error", func() {
-			bodyMock := mocks.NewMockReadCloser(mockCtrl)
-			bodyMock.EXPECT().Read(gomock.Any()).Return(0, errors.New("some error"))
-			bodyMock.EXPECT().Close()
-			resp := &http.Response{
-				StatusCode: 200,
-				Body:       bodyMock,
-			}
-			mockClient.EXPECT().Get(gomock.Any()).Return(resp, nil)
-			events, err := usageEvents.Get(GUIDNil, 10, 0)
-			Expect(err.Error()).ToNot(BeNil())
-			Expect(events).To(BeNil())
-		})
-
-	})
-
-	Context("When the method is not used correctly", func() {
-
-		It("should panic if GUID is empty", func() {
-			Expect(func() { usageEvents.Get("", 10, 0) }).To(Panic())
-		})
-
-	})
-
-})
+var _ = Describe("The Service Usage Events Handler", usageEventTests("service", func(client Client, logger lager.Logger) UsageEventsAPI {
+	return NewServiceUsageEventsAPI(client, logger)
+}))
