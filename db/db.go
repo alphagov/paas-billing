be8075803bda1d37772259ef94161ade66b36d24
Dan Carley <dan.carley@gmail.com>
2018-01-16 11:55:04 +0000

    Revert "Add CONCURRENTLY to REFRESH MATERIALIZED VIEW"
    This reverts commit bcdbd3bbe36e0277e4c9e1f6756b553203d6ef20.

--- db/db.go
+++ db/db.go
@@ -91,7 +91,7 @@ func (pc *PostgresClient) FetchLastGUID(tableName string) (string, error) {
 
 // UpdateViews updates the indexed materialized views used to generate reports
 func (pc *PostgresClient) UpdateViews() error {
-	_, err := pc.Conn.Exec("REFRESH MATERIALIZED VIEW CONCURRENTLY billable")
+	_, err := pc.Conn.Exec("REFRESH MATERIALIZED VIEW billable")
 	return err
 }
 
