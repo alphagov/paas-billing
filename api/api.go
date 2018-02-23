2101d29a518711057f8b850f5356eb525a066fbe
bandesz <bandesz@gmail.com>
2018-02-23 14:16:31 +0000

    Update usage queries to return with original results (without breakdown)

--- api/api.go
+++ api/api.go
@@ -19,21 +19,39 @@ const billableViewName = "billable"
 func NewUsageHandler(db db.SQLClient) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return withAuthorizedResources(Many, billableViewName, c, db, `
+			with
+			summed_resources as (
+				select
+					guid,
+					id,
+					pricing_plan_id,
+					min(pricing_plan_component_id) as pricing_plan_component_id,
+					sum(price::bigint) as price
+				from
+					monetized_resources
+				group by
+					guid, id, pricing_plan_id
+			)
 			select
-				guid,
-				org_guid,
-				space_guid,
-				pricing_plan_id,
-				pricing_plan_name,
-				name,
-				memory_in_mb,
-				iso8601(lower(duration)) as start,
-				iso8601(upper(duration)) as stop,
-				price::bigint as price
+				mr.guid,
+				mr.org_guid,
+				mr.space_guid,
+				mr.pricing_plan_id,
+				mr.pricing_plan_name,
+				mr.name,
+				mr.memory_in_mb,
+				iso8601(lower(mr.duration)) as start,
+				iso8601(upper(mr.duration)) as stop,
+				sr.price::bigint
 			from
-				monetized_resources
+				summed_resources sr
+			inner join
+				monetized_resources mr
+			on
+				sr.id = mr.id
+				and sr.pricing_plan_component_id = mr.pricing_plan_component_id
 			order by
-				guid, id
+				mr.guid, mr.id, mr.pricing_plan_id
 		`)
 	}
 }
@@ -325,21 +343,38 @@ func ListEventUsageForResource(db db.SQLClient) echo.HandlerFunc {
 			return errors.New("missing resource_guid")
 		}
 		return withAuthorizedResources(Many, billableViewName, c, db, `
+			with
+			summed_resources as (
+				select
+					id,
+					pricing_plan_id,
+					min(pricing_plan_component_id) as pricing_plan_component_id,
+					sum((price * 100)::bigint) as price_in_pence
+				from
+					monetized_resources
+				where
+					guid = $1
+				group by
+					id, pricing_plan_id
+			)
 			select
-				guid,
-				org_guid,
-				space_guid,
-				pricing_plan_id,
-				pricing_plan_name,
-				iso8601(lower(duration)) as from,
-				iso8601(upper(duration)) as to,
-				(price * 100)::bigint as price_in_pence
+				mr.guid,
+				mr.org_guid,
+				mr.space_guid,
+				mr.pricing_plan_id,
+				mr.pricing_plan_name,
+				iso8601(lower(mr.duration)) as from,
+				iso8601(upper(mr.duration)) as to,
+				sr.price_in_pence::bigint
 			from
-				monetized_resources
-			where
-				guid = $1
+				summed_resources sr
+			inner join
+				monetized_resources mr
+			on
+				mr.id = sr.id
+				and mr.pricing_plan_component_id = sr.pricing_plan_component_id
 			order by
-				id, pricing_plan_id, guid
+				sr.id, sr.pricing_plan_id
 		`, resourceGUID)
 	}
 }
@@ -347,19 +382,39 @@ func ListEventUsageForResource(db db.SQLClient) echo.HandlerFunc {
 func ListEventUsage(db db.SQLClient) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return withAuthorizedResources(Many, billableViewName, c, db, `
+			with
+			summed_resources as (
+				select
+					guid,
+					id,
+					pricing_plan_id,
+					min(pricing_plan_component_id) as pricing_plan_component_id,
+					sum(price::bigint) as price
+				from
+					monetized_resources
+				group by
+					guid, id, pricing_plan_id
+			)
 			select
-				guid,
-				org_guid,
-				space_guid,
-				pricing_plan_id,
-				pricing_plan_name,
-				iso8601(lower(duration)) as from,
-				iso8601(upper(duration)) as to,
-				(price * 100)::bigint as price_in_pence
+				mr.guid,
+				mr.org_guid,
+				mr.space_guid,
+				mr.pricing_plan_id,
+				mr.pricing_plan_name,
+				mr.name,
+				mr.memory_in_mb,
+				iso8601(lower(mr.duration)) as from,
+				iso8601(upper(mr.duration)) as to,
+				sr.price::bigint
 			from
-				monetized_resources
+				summed_resources sr
+			inner join
+				monetized_resources mr
+			on
+				sr.id = mr.id
+				and sr.pricing_plan_component_id = mr.pricing_plan_component_id
 			order by
-				id, pricing_plan_id, guid
+				sr.guid, sr.id, sr.pricing_plan_id
 		`)
 	}
 }
