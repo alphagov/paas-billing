24cbe80a64abfb28c1107151ba86134741eaa15b
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2018-04-25 14:28:03 +0100

    eventcollector

---
+++ eventcollector/collector_suite_test.go
@@ -0,0 +1,13 @@
+package eventcollector_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCollector(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "EventCollector")
+}
