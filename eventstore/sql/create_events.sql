bf38c476b2bcc04b5358034aa614895693d72b36
Dan Carley <dan.carley@gmail.com>
2018-05-22 13:47:01 +0100

    Replace pg_size_bytes function
    With upstream backport from https://github.com/chrisfarms/pg_size_bytes
    
    Needs some more tests, probably table tests for the function.

--- eventstore/sql/create_events.sql
+++ eventstore/sql/create_events.sql
@@ -16,25 +16,39 @@ CREATE TABLE events (
 );
 
 -- FIXME: should be dropped once we'd upgrade postgres version.
-CREATE FUNCTION pg_size_bytes (size text)
-	RETURNS bigint
-AS $$
-  if (size[-1] == 'B'):
-    size = size[:-1]
-  value = size[:-1].strip()
-  unit = size[-1]
-  if (value.isdigit()):
-    b = int(value)
-    if (unit == 'G'):
-      b *= 1073741824
-    elif (unit == 'M'):
-      b *= 1048576
-    elif (unit == 'K'):
-      b *= 1024
-  else:
-    b = 0
-  return b
-$$ LANGUAGE plv8;
+create or replace function pg_size_bytes(input text) returns bigint as $$ declare
+	size text;
+	unit text := 'bytes';
+	value numeric := 0;
+	n bigint;
+begin
+	size = trim(regexp_replace(input, E'\\s+', ' ', 'g'));
+	unit = lower(regexp_replace(size, E'[\\-\\.0-9]', '', 'g'));
+	value = array_to_string(regexp_matches(size, E'^([0-9e\\-\\.]+)', 'i'), '')::numeric;
+	if value is null then
+		raise exception 'invalid size "%"', input;
+	end if;
+	if unit ~* 'bytes$' or unit = '' then
+		value = value;
+	elsif unit ~* 'kb$' then
+		value = value * 1024;
+	elsif unit ~* 'mb$' then
+		value = value * 1024 * 1024;
+	elsif unit ~* 'gb$' then
+		value = value * 1024 * 1024 * 1024;
+	elsif unit ~* 'tb$' then
+		value = value * 1024 * 1024 * 1024 * 1024;
+	else
+		n = value::bigint;
+		value = null;
+	end if;
+	if value is null then
+		raise exception 'invalid size "%"', input using
+			hint = 'Valid units are "bytes", "kB", "MB", "GB", and "TB"',
+			detail = 'Invalid size unit: "' || unit || '"';
+	end if;
+	return value::bigint;
+end; $$ language plpgsql;
 -- FIXME: END
 
 -- extract useful stuff from usage events
