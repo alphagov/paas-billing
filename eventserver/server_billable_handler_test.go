ded36fa9edd467c5c3ebeff2378fdd423317f695
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-09-05 16:43:50 +0100

    /billable_events returns X month consolidated events
    Return consolidated events if month is consolidated
    
    We're now iterating over all the months in the requested range and
    checking if that month has been consolidated.
    
    Note: This might change the values due rounding errors, as
    now we are splitting all the events in the month boundary.

--- eventserver/server_billable_handler_test.go
+++ eventserver/server_billable_handler_test.go
@@ -13,6 +13,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+
 	"github.com/alphagov/paas-billing/eventio"
 	. "github.com/alphagov/paas-billing/eventserver"
 	. "github.com/onsi/ginkgo"
@@ -225,6 +226,53 @@ var _ = Describe("BillableEventsHandler", func() {
 		Expect(res.Header().Get("Content-Type")).To(Equal("application/json; charset=UTF-8"))
 	})
 
+	It("should fetch ConsolidatedBillableEvents for whole months which have been consolidated and billable events otherwise", func() {
+		fakeAuthenticator.NewAuthorizerReturns(fakeAuthorizer, nil)
+		fakeAuthorizer.AdminReturns(false, nil)
+		fakeAuthorizer.HasBillingAccessReturns(true, nil)
+		fakeRows := &fakes.FakeBillableEventRows{}
+		fakeRows.CloseReturns(nil)
+		fakeRows.NextReturns(false)
+		fakeStore.IsRangeConsolidatedReturnsOnCall(0, false, nil)
+		fakeStore.IsRangeConsolidatedReturnsOnCall(1, true, nil)
+		fakeStore.IsRangeConsolidatedReturnsOnCall(2, true, nil)
+		fakeStore.IsRangeConsolidatedReturnsOnCall(3, false, nil)
+		fakeStore.GetBillableEventRowsReturns(fakeRows, nil)
+		fakeStore.GetConsolidatedBillableEventRowsReturns(fakeRows, nil)
+
+		u := url.URL{}
+		u.Path = "/billable_events"
+		q := u.Query()
+		q.Set("org_guid", orgGUID1)
+		q.Set("range_start", "2001-01-15")
+		q.Set("range_stop", "2001-04-15")
+		u.RawQuery = q.Encode()
+		req := httptest.NewRequest(echo.GET, u.String(), nil)
+		req.Header.Set("Authorization", "bearer "+token)
+		res := httptest.NewRecorder()
+
+		e := New(cfg)
+		e.ServeHTTP(res, req)
+		defer e.Shutdown(ctx)
+
+		Expect(fakeStore.GetBillableEventRowsCallCount()).To(Equal(2))
+		Expect(fakeStore.GetConsolidatedBillableEventRowsCallCount()).To(Equal(2))
+		receivedFilters := []eventio.EventFilter{
+			fakeStore.GetBillableEventRowsArgsForCall(0),
+			fakeStore.GetConsolidatedBillableEventRowsArgsForCall(0),
+			fakeStore.GetConsolidatedBillableEventRowsArgsForCall(1),
+			fakeStore.GetBillableEventRowsArgsForCall(1),
+		}
+		Expect(receivedFilters).To(Equal([]eventio.EventFilter{
+			{RangeStart: "2001-01-15", RangeStop: "2001-02-01", OrgGUIDs: []string{orgGUID1}},
+			{RangeStart: "2001-02-01", RangeStop: "2001-03-01", OrgGUIDs: []string{orgGUID1}},
+			{RangeStart: "2001-03-01", RangeStop: "2001-04-01", OrgGUIDs: []string{orgGUID1}},
+			{RangeStart: "2001-04-01", RangeStop: "2001-04-15", OrgGUIDs: []string{orgGUID1}},
+		}))
+
+		Expect(res.Code).To(Equal(200))
+		Expect(res.Header().Get("Content-Type")).To(Equal("application/json; charset=UTF-8"))
+	})
 	It("should fetch ConsolidatedBillableEvents when the filter range has been consolidated", func() {
 		fakeAuthenticator.NewAuthorizerReturns(fakeAuthorizer, nil)
 		fakeAuthorizer.AdminReturns(false, nil)
@@ -311,25 +359,43 @@ var _ = Describe("BillableEventsHandler", func() {
 var _ = Describe("WriteRowsAsJson", func() {
 	It("Should write out an empty array when there are no rows", func() {
 		b := &FlushyBuffer{bytes.Buffer{}}
-		events := []eventio.BillableEvent{}
-		rows := &FakeRows{contents: events}
-		Expect(WriteRowsAsJson(b, b, rows)).To(Succeed())
+		Expect(WriteRowsAsJson(b, b, &RowOfRows{})).To(Succeed())
 		Expect(b.String()).To(MatchJSON(`[]`))
 	})
 
-	It("Should write an array with several objects when there are several rows", func() {
+	It("Should write an array with one item when there is one row", func() {
 		b := &FlushyBuffer{bytes.Buffer{}}
-		events := []eventio.BillableEvent{
-			{EventGUID: "some-event-guid-1"},
-			{EventGUID: "some-event-guid-2"},
-			{EventGUID: "some-event-guid-3"},
-			{EventGUID: "some-event-guid-4"},
-		}
-		rows := &FakeRows{contents: events}
-		Expect(WriteRowsAsJson(b, b, rows)).To(Succeed())
+		events := []eventio.BillableEvent{{EventGUID: "some-event-guid"}}
+		rows := FakeRows{contents: events}
+		rowsCollection := []eventio.BillableEventRows{&rows}
+		Expect(WriteRowsAsJson(b, b, &RowOfRows{RowsCollection: rowsCollection})).To(Succeed())
 		writtenEvents := []eventio.BillableEvent{}
 		Expect(json.Unmarshal(b.Bytes(), &writtenEvents)).To(Succeed())
 		Expect(writtenEvents).To(Equal(events))
+	})
+
+	It("Should write an array with two items when there is one set of rows with two events", func() {
+		b := &FlushyBuffer{bytes.Buffer{}}
+		events := []eventio.BillableEvent{{EventGUID: "some-event-guid"}, {EventGUID: "some-other-event-guid"}}
+		rows := FakeRows{contents: events}
+		rowsCollection := []eventio.BillableEventRows{&rows}
+		Expect(WriteRowsAsJson(b, b, &RowOfRows{RowsCollection: rowsCollection})).To(Succeed())
+		writtenEvents := []eventio.BillableEvent{}
+		Expect(json.Unmarshal(b.Bytes(), &writtenEvents)).To(Succeed())
+		Expect(writtenEvents).To(Equal(events))
+	})
+
+	It("Should write an array with two items when there are two sets of rows with one event each", func() {
+		b := &FlushyBuffer{bytes.Buffer{}}
+		events := []eventio.BillableEvent{{EventGUID: "some-event-guid"}}
+		rowsOne := FakeRows{contents: events}
+		rowsTwo := FakeRows{contents: events}
+		rowsCollection := []eventio.BillableEventRows{&rowsOne, &rowsTwo}
+		Expect(WriteRowsAsJson(b, b, &RowOfRows{RowsCollection: rowsCollection})).To(Succeed())
+		writtenEvents := []eventio.BillableEvent{}
+		bytes := b.Bytes()
+		Expect(json.Unmarshal(bytes, &writtenEvents)).To(Succeed(), "Couldn't parse JSON: "+string(bytes))
+		Expect(writtenEvents).To(Equal(append(events, events...)))
 	})
 })
 
