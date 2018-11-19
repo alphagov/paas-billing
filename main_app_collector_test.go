a145f103aa1334cfa9747c84faad782ea674921c
Jonathan Matthews <jonathan.matthews@digital.cabinet-office.gov.uk>
2018-11-19 11:09:48 +0000

    Split the API server and event collector
    We need to make the API server blue-green-deployable. This means that it
    needs to be possible to run it twice, simultaneously, against the same
    CF.
    
    This is a problem within the current architecture as the collector and
    the API both run in the same process. We can't allow the collector to
    run more than once as we're unsure if this might have an effect on the
    bills produced.
    
    The net result of this is that we need to split this application into 2,
    the API and the collector, so that we can blue-green-deploy the API
    portion whilst leaving the deployment of the collector as a simple `cf
    push` (which results in no more than one instance running).
    
    We achieve this here by adding a second-level noun to the program's
    invocation and starting only those sections of the program as are
    required.
    
    To use this: where previously you ran `./path/to/paas-billing`, now to
    start the collector you should run `./path/to/paas-billing collector`
    and to start the api/event server you should run `./path/to/paas-billing
    api`.
    
    It is not possible to run both components at once.

---
+++ main_app_collector_test.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"context"
+	"sync"
+
+	"code.cloudfoundry.org/lager"
+	"github.com/alphagov/paas-billing/fakes"
+	"github.com/alphagov/paas-billing/testenv"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = It("Should perform a smoke test against a real environment", func() {
+	enabled := os.Getenv("ENABLE_SMOKE_TESTS")
+	if enabled != "true" {
+		Skip("smoke tests are disabled set ENABLE_SMOKE_TESTS=true to enable them")
+	}
+
+	var (
+		err                 error
+		session             *Session
+		tempDB              *testenv.TempDB
+		anOrgGUIDWithEvents string
+		validAuthToken      = os.Getenv("TEST_AUTH_TOKEN")
+		//httpClient          = &http.Client{}
+		port = "8765"
+	)
+
+	By("Setting up a environment", func() {
+		tempDB, err = testenv.New()
+		Expect(err).ToNot(HaveOccurred())
+		os.Setenv("DATABASE_URL", tempDB.TempConnectionString)
+		os.Setenv("COLLECTOR_MIN_WAIT_TIME", "100ms")
+		os.Setenv("PROCESSOR_SCHEDULE", "5s")
+		os.Setenv("PORT", port)
+	})
+
+	defer By("Removing the temp database environment", func() {
+		tempDB.Close()
+	})
+
+	By("Ensuring a TEST_AUTH_TOKEN environment variable is set", func() {
+		Expect(validAuthToken).ToNot(BeEmpty(), "TEST_AUTH_TOKEN must be set to a valid cf oauth-token for this test")
+	})
+
+	By("Starting the app", func() {
+		cmd := exec.Command(BinaryPath, "collector")
+		cmd.Env = os.Environ()
+		session, err = Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(session.Out, 10*time.Second).Should(Say("paas-billing.starting"))
+	})
+
+	defer By("Killing the app (if it hasn't already been shutdown)", func() {
+		session.Kill()
+	})
+
+	By("Waiting for the EventStore to report it has been initialized", func() {
+		Eventually(session.Out, 20*time.Second).Should(Say("paas-billing.store.initializing"))
+		Eventually(session.Out, 60*time.Second).Should(Say("paas-billing.store.initialized"))
+	})
+
+	By("Waiting for the HistoricDataStore to report it has been initialized", func() {
+		Eventually(session.Out, 60*time.Second).Should(Say("paas-billing.historic-data-store.initialized"))
+	})
+
+	By("Ensuring Service/ServicePlan data exists after HistoricDataStore.Init", func() {
+		Expect(
+			tempDB.Get(`select count(*) from service_plans`),
+		).To(BeNumerically(">", 0), "expected some service_plans to be collected during init")
+		Expect(
+			tempDB.Get(`select count(*) from services`),
+		).To(BeNumerically(">", 0), "expected some services to be collected during init")
+	})
+
+	By("Waiting for some raw events to be collected in the database", func() {
+		Eventually(func() interface{} {
+			return tempDB.Get(`select count(*) from app_usage_events`)
+		}, 1*time.Minute).Should(BeNumerically(">", 0), "expected some app_usage_events to be collected")
+	})
+
+	By("Waiting for some events to be processed in the database", func() {
+		Eventually(func() interface{} {
+			return tempDB.Get(`select count(*) from events`)
+		}, 5*time.Minute).Should(BeNumerically(">", 0), "expected some events to processed")
+		anOrgGUIDWithEvents = tempDB.Get(`select org_guid::text from events limit 1`).(string)
+		Expect(anOrgGUIDWithEvents).ToNot(BeEmpty())
+	})
+
+	By("Sending SIGTERM to the process", func() {
+		session.Terminate()
+		Eventually(session.Out, 10*time.Second).Should(Say("paas-billing.stopping"))
+	})
+
+	By("Waiting until the process exits cleanly", func() {
+		Eventually(session, 60*time.Second).Should(Exit(0))
+	})
+})
+
+var _ = Describe("runRefreshAndConsolidateLoop", func() {
+	var (
+		fakeStore *fakes.FakeEventStore
+		logger    lager.Logger
+	)
+
+	BeforeEach(func() {
+		fakeStore = &fakes.FakeEventStore{}
+		logger = lager.NewLogger("test")
+	})
+
+	It("should call Refresh and Consolidate every 'Schedule'", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		wg := sync.WaitGroup{}
+		defer wg.Wait()
+		defer cancel()
+
+		go func() {
+			wg.Add(1)
+			runRefreshAndConsolidateLoop(ctx, logger, 1*time.Nanosecond, fakeStore)
+			wg.Done()
+		}()
+
+		Eventually(func() int {
+			return fakeStore.RefreshCallCount()
+		}).Should(BeNumerically(">=", 1))
+
+		Eventually(func() int {
+			return fakeStore.ConsolidateAllCallCount()
+		}).Should(BeNumerically(">=", 1))
+	})
+
+	It("should not call Consolidate if Refresh fails", func() {
+		fakeStore.RefreshReturns(fmt.Errorf("some-error"))
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		wg := sync.WaitGroup{}
+		defer wg.Wait()
+		defer cancel()
+
+		go func() {
+			wg.Add(1)
+			runRefreshAndConsolidateLoop(ctx, logger, 1*time.Nanosecond, fakeStore)
+			wg.Done()
+		}()
+
+		Eventually(func() int {
+			return fakeStore.RefreshCallCount()
+		}).Should(BeNumerically(">=", 2))
+
+		Consistently(func() int {
+			return fakeStore.ConsolidateAllCallCount()
+		}).Should(BeNumerically("==", 0))
+	})
+})
