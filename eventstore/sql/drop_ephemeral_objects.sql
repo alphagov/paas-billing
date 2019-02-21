ccc80d9ca7fbf7fafdd27678f9bf3a064d058b11
bandesz <bandesz@gmail.com>
2019-02-21 14:29:29 +0000

    Remove DROP commands for legacy views

--- eventstore/sql/drop_ephemeral_objects.sql
+++ eventstore/sql/drop_ephemeral_objects.sql
@@ -1,5 +1,3 @@
-DROP MATERIALIZED VIEW IF EXISTS billable;           -- FIXME legacy: remove me later
-DROP MATERIALIZED VIEW IF EXISTS resource_durations;           -- FIXME legacy: remove me later
 DROP TABLE IF EXISTS pricing_plan_components;
 DROP TABLE IF EXISTS pricing_plans;
 DROP TABLE IF EXISTS vat_rates;
