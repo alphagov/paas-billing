67d3b4a12da409fd7e9be1f452cf3df30e166dcf
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-08-28 12:14:50 +0100

    app_usage_events, service_usage_events: add index on raw_message->>'space_name' not being ignorable
    in an attempt to speed up billing query

--- eventstore/sql/create_app_usage_events.sql
+++ eventstore/sql/create_app_usage_events.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS app_usage_events (
 CREATE INDEX IF NOT EXISTS app_usage_id_idx ON app_usage_events (id);
 CREATE INDEX IF NOT EXISTS app_usage_state_idx ON app_usage_events ( (raw_message->>'state') );
 CREATE INDEX IF NOT EXISTS app_usage_space_name_idx ON app_usage_events ( (raw_message->>'space_name') text_pattern_ops);
+CREATE INDEX IF NOT EXISTS app_usage_space_name_not_unbilled_idx ON app_usage_events ( (raw_message->>'space_name' !~ '^(SMOKE|ACC|CATS|PERF)-') );
 
 DO $$ BEGIN
 	ALTER TABLE app_usage_events ADD CONSTRAINT created_at_not_zero_value CHECK (created_at > 'epoch'::timestamptz);
--- eventstore/sql/create_service_usage_events.sql
+++ eventstore/sql/create_service_usage_events.sql
@@ -9,3 +9,4 @@ CREATE INDEX IF NOT EXISTS service_usage_id_idx ON service_usage_events (id);
 CREATE INDEX IF NOT EXISTS service_usage_state_idx ON service_usage_events ( (raw_message->>'state') );
 CREATE INDEX IF NOT EXISTS service_usage_type_idx ON service_usage_events ( (raw_message->>'service_instance_type') );
 CREATE INDEX IF NOT EXISTS service_usage_space_name_idx ON service_usage_events ( (raw_message->>'space_name') text_pattern_ops);
+CREATE INDEX IF NOT EXISTS service_usage_space_name_not_unbilled_idx ON service_usage_events ( (raw_message->>'space_name' !~ '^(SMOKE|ACC|CATS|PERF)-') );
