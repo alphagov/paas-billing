360437524cc8e0199a4c6d2add305bd41bfacfa7
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2021-05-18 15:31:31 +0100

    Log actions with Lager
    Runs logging throughout the performance data exporter. It
    currently clashes with the data itself on stdout, but in the
    future the data will be written to a remote source instead.

--- performance_data_exporter/go.mod
+++ performance_data_exporter/go.mod
@@ -3,7 +3,9 @@ module github.com/alphagov/paas-billing/performance_data_exporter
 go 1.15
 
 require (
+	code.cloudfoundry.org/lager v2.0.0+incompatible
 	github.com/joho/sqltocsv v0.0.0-20210428211105-a6d6801d59df
 	github.com/lib/pq v1.10.2
+	github.com/onsi/gomega v1.12.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1
 )
--- performance_data_exporter/main.go
+++ performance_data_exporter/main.go
@@ -3,39 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/joho/sqltocsv"
-	_ "github.com/lib/pq"
-	"github.com/robfig/cron/v3"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/signal"
 	"strings"
+
+	"code.cloudfoundry.org/lager"
+	"github.com/joho/sqltocsv"
+	_ "github.com/lib/pq"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
+	logger := lager.NewLogger("performance-data-exporter")
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.INFO))
+	logger.Info("startup")
 	connectionString, found := os.LookupEnv("DATABASE_URL")
 	if !found {
-		log.Fatalln("DATABASE_URL environment variable must be set")
+		logger.Error("startup", fmt.Errorf("DATABASE_URL environment variable must be set"))
+		os.Exit(1)
 	}
 
 	exportFrequency, found := os.LookupEnv("EXPORT_FREQUENCY")
 	if !found {
-		log.Fatalln("EXPORT_FREQUENCY environment variable must be set")
+		logger.Error("startup", fmt.Errorf("EXPORT_FREQUENCY environment variable must be set"))
+		os.Exit(1)
 	}
 
 	workingDir, _ := os.Getwd()
-
 	scheduler := cron.New(cron.WithChain(
-		cron.Recover(cron.DefaultLogger),  // or use cron.DefaultLogger
+		cron.Recover(cron.DefaultLogger), // or use cron.DefaultLogger
 	))
+
+	logger.Info("add-to-schedule", lager.Data{"schedule": exportFrequency})
 	_, err := scheduler.AddFunc(exportFrequency, func() {
-		exportCSVToStdOut(connectionString, workingDir)
+		logSess := logger.Session("exporter-run")
+		exportCSVToStdOut(connectionString, workingDir, logSess)
 	})
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("add-to-schedule", err)
+		os.Exit(1)
 	}
 
+	logger.Info("begin-schedule")
 	scheduler.Start()
 	waitForExit()
 }
@@ -46,51 +56,72 @@ func waitForExit() os.Signal {
 	return <-c
 }
 
-func exportCSVToStdOut(connectionString string, workingDir string) {
+func exportCSVToStdOut(connectionString string, workingDir string, logger lager.Logger) {
+	logger.Info("connect-to-db")
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("connect-to-db", err)
+		return
 	}
-	defer db.Close()
-	defer cleanup(db, workingDir)
+	defer (func(){
+		logger.Info("close-db")
+		err := db.Close()
+		if err != nil {
+			logger.Error("close-db", err)
+		}
+	})()
+	defer cleanup(db, workingDir, logger)
 
 	recreateFileBytes, err := ioutil.ReadFile(workingDir + "/sql/usage-and-adoption-recreate.sql")
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("read-table-create-file", err)
+		return
 	}
+
+	logger.Info("create-database-tables")
 	recreateString := string(recreateFileBytes)
 	_, err = db.Exec(recreateString)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("create-database-tables", err)
+		return
 	}
 
 	queryFileBytes, err := ioutil.ReadFile(workingDir + "/sql/usage-and-adoption-generate-csv.sql")
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("read-query-file", err)
+		return
 	}
 
+	logger.Error("run-query", err)
 	queryString := string(queryFileBytes)
 	rows, err := db.Query(queryString)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("run-query", err)
+		return
 	}
 
+	logger.Info("write-to-stdout")
 	writer := strings.Builder{}
 	err = sqltocsv.Write(&writer, rows)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("write-to-stdout", err)
+		return
 	}
 	fmt.Printf("%v", writer.String())
 }
 
-func cleanup(db *sql.DB, workingDir string) {
+func cleanup(db *sql.DB, workingDir string, logger lager.Logger) {
 	cleanupFileBytes, err := ioutil.ReadFile(workingDir + "/sql/usage-and-adoption-recreate.sql")
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("read-cleanup-file", err)
+		return
 	}
 	cleanupString := string(cleanupFileBytes)
+
+	logger.Info("perform-database-cleanup")
 	_, err = db.Exec(cleanupString)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("perform-database-cleanup", err)
+		return
 	}
 }
