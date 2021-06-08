5cdc3db4528b7629bc9c33dffb83fab188b8b418
Sebastian Schmieschek <sebastian.schmieschek@digital.cabinet-office.gov.uk>
2021-06-08 21:46:13 +0100

    REVERT ME: Changes for comparability
    We need to compare records to the regular update script and the
    last_updated field will always differ if not fixed.
    
    Also rename created table to resources_reference.
    
    Also Filter additional acceptance test events.

--- billing-db/migration_scripts/resources.sql
+++ billing-db/migration_scripts/resources.sql
@@ -1,4 +1,4 @@
-INSERT INTO resources
+INSERT INTO resources_reference
 SELECT  DISTINCT LOWER(duration) AS "valid_from",
         UPPER(duration) AS "valid_to",
         resource_guid,
@@ -14,5 +14,6 @@ SELECT  DISTINCT LOWER(duration) AS "valid_from",
         memory_in_mb,
         storage_in_mb,
         event_guid AS "cf_event_guid",
-        NOW()
+        TIMESTAMPTZ('2021-06-08T00:00:00') AS last_updated
 FROM billable_event_components;
+WHERE space_name != '^(SMOKE|ACC|CATS|PERF|BACC|AIVENBACC|ASATS)'
