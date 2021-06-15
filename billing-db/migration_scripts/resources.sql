dd68bd57196e6f8ad74157466ef279d092010399
poveyd <dave_povey@hotmail.com>
2021-06-15 10:40:35 +0100

    Add column list

--- billing-db/migration_scripts/resources.sql
+++ billing-db/migration_scripts/resources.sql
@@ -1,4 +1,22 @@
 INSERT INTO resources_reference
+(
+    valid_from,
+    valid_to,
+    resource_guid,
+    resource_name,
+    resource_type,
+    org_guid,
+    org_name,
+    space_guid,
+    space_name,
+    plan_name,
+    plan_guid,
+    storage_in_mb,
+    memory_in_mb,
+    number_of_nodes,
+    cf_event_guid,
+    last_updated
+)
 SELECT  DISTINCT LOWER(duration) AS "valid_from",
         UPPER(duration) AS "valid_to",
         resource_guid,
@@ -10,10 +28,10 @@ SELECT  DISTINCT LOWER(duration) AS "valid_from",
         space_name,
         plan_name,
         plan_guid,
-        number_of_nodes,
-        memory_in_mb,
         storage_in_mb,
+        memory_in_mb,
+        number_of_nodes,
         event_guid AS "cf_event_guid",
-        TIMESTAMPTZ('2021-06-08T00:00:00') AS last_updated
-FROM billable_event_components;
-WHERE space_name != '^(SMOKE|ACC|CATS|PERF|BACC|AIVENBACC|ASATS)'
+        TIMESTAMPTZ('2021-06-08T00:00:00') AS "last_updated"
+FROM billable_event_components
+WHERE space_name != '^(SMOKE|ACC|CATS|PERF|BACC|AIVENBACC|ASATS)';
