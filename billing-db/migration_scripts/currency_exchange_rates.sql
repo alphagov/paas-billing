14986141ec3754e145fc52bc74467e621a364fda
poveyd <dave_povey@hotmail.com>
2021-06-25 11:38:29 +0100

    Add mapping from GBP to GBP. Having this mapping simplifies calculate_bill() code

--- billing-db/migration_scripts/currency_exchange_rates.sql
+++ billing-db/migration_scripts/currency_exchange_rates.sql
@@ -1,1 +1,2 @@
 INSERT INTO currency_exchange_rates (from_ccy, to_ccy, valid_from, valid_to, rate) SELECT 'USD', 'GBP', '1970-01-01', '9999-12-31', 0.8;
+INSERT INTO currency_exchange_rates (from_ccy, to_ccy, valid_from, valid_to, rate) SELECT 'GBP', 'GBP', '1970-01-01', '9999-12-31', 1.0;
