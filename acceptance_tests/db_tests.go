a940747283923ed09d6db17c1e8135dce7a93805
Sebastian Schmieschek <sebastian.schmieschek@digital.cabinet-office.gov.uk>
2021-08-05 17:23:56 +0100

    Add tests for resource creation from service usage
    Initial test for a simple created and deleted service instance.

--- acceptance_tests/db_tests.go
+++ acceptance_tests/db_tests.go
@@ -778,7 +778,8 @@ var _ = Describe("BillingSQLFunctions", func() {
 			},
 		}))
 	})
-	It("Correctly updates resources based on app usage events", func() {
+// TODO need to include TASKS, etc here?
+  It("Correctly updates resources based on app usage events", func() {
 		db, err := testenv.Open(eventstore.Config{})
 		Expect(err).ToNot(HaveOccurred())
     // TODO: uncomment the below
@@ -850,6 +851,123 @@ var _ = Describe("BillingSQLFunctions", func() {
         "number_of_nodes": 2,
         "cf_event_guid":   "b84b96e3-ea99-46d0-9520-76c2f40efff7",
       },
+		}))
+	})
+  It("Ignores app usage events triggered by tests", func() {
+		db, err := testenv.Open(eventstore.Config{})
+		Expect(err).ToNot(HaveOccurred())
+    // TODO: uncomment the below
+    //defer db.Close()
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "1",
+        "guid":              "b6253aa7-ce44-4a2a-a9c2-f26a8c3b2c91",
+				"created_at":        "2021-07-01T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"SMOKE-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "2",
+        "guid":              "59be2b7f-3554-4f54-8dcb-100b0b248321",
+				"created_at":        "2021-07-02T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"ACC-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "3",
+        "guid":              "be98110f-8397-48fd-a6aa-77e180ea8a3e",
+				"created_at":        "2021-07-03T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"CATS-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "4",
+        "guid":              "b2395dc0-4b88-4b3a-a782-21badce9bcb5",
+				"created_at":        "2021-07-04T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"PERF-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "5",
+        "guid":              "0b695683-c896-443a-8a8b-754cef460163",
+				"created_at":        "2021-07-05T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"BACC-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "6",
+        "guid":              "3b080789-3a43-4768-859f-fe082177fc62",
+				"created_at":        "2021-07-06T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"AIVENBACC-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Insert("app_usage_events",
+			testenv.Row{
+        "id":                "7",
+        "guid":              "8e83f645-1a19-48e4-abb7-29ae71941db7",
+				"created_at":        "2021-07-07T00:00:00Z",
+				"raw_message":       "{\"state\": \"STARTED\", \"app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"org_guid\": \"428d5022-3ea5-46e9-8220-fc1e80b58de5\", \"task_guid\": null, \"task_name\": null, \"space_guid\": \"c9bbfb98-9429-4c58-a57f-4304ef7f30a2\", \"space_name\": \"ASATS-1-SPACE-1c5968cee02f3899\", \"process_type\": \"web\", \"package_state\": \"STAGED\", \"buildpack_guid\": \"60b5ec15-3db4-4554-8cb0-4be2bcb64526\", \"buildpack_name\": \"binary_buildpack\", \"instance_count\": 1, \"previous_state\": \"STOPPED\", \"parent_app_guid\": \"12a71e81-8cbf-4d46-bfa5-a5d446735f73\", \"parent_app_name\": \"unit-test-APP-c83c773e9daf5af3\", \"previous_package_state\": \"UNKNOWN\", \"previous_instance_count\": 1, \"memory_in_mb_per_instance\": 30, \"previous_memory_in_mb_per_instance\": 30}",
+			})).To(Succeed())
+
+    Expect(db.Query(`select * from update_resources('1970-01-01T00:00:00Z')`)).To(MatchJSON(testenv.Rows{
+        {
+        "num_rows_added": 0,
+        },
+      }))
+	})
+  It("Correctly updates resources based on service usage events", func() {
+		db, err := testenv.Open(eventstore.Config{})
+		Expect(err).ToNot(HaveOccurred())
+    // TODO: uncomment the below
+    //defer db.Close()
+
+    Expect(db.Insert("service_usage_events",
+			testenv.Row{
+        "id":                "1",
+        "guid":              "b6253aa7-ce44-4a2a-a9c2-f26a8c3b2c91",
+				"created_at":        "2021-07-01T00:00:00Z",
+				"raw_message":       "{\"state\": \"CREATED\", \"org_guid\": \"53d03a41-16a7-40a3-b1a9-83298e65f8f5\", \"space_guid\": \"1cfdea3c-af34-4950-8383-5492e7c86c36\", \"space_name\": \"test-SPACE-dc424ba9f99abbf5\", \"service_guid\": \"7ab41178-f5a2-4444-b2f6-71abf43708b2\", \"service_label\": \"postgres\", \"service_plan_guid\": \"5eff64fa-8b41-4e6b-9321-b81624c678ce\", \"service_plan_name\": \"tiny-unencrypted-12\", \"service_broker_guid\": \"5cb906f6-ac20-4393-9210-e783fb4c8f39\", \"service_broker_name\": \"rds-broker\", \"service_instance_guid\": \"c64e2c78-db40-44ae-987c-f2171fe5e42d\", \"service_instance_name\": \"BACC-4-test-db-74b2529697d9ed58\", \"service_instance_type\": \"managed_service_instance\"}",
+			})).To(Succeed())
+
+    Expect(db.Insert("service_usage_events",
+			testenv.Row{
+        "id":                "2",
+        "guid":              "b84b96e3-ea99-46d0-9520-76c2f40efff7",
+				"created_at":        "2021-07-15T00:00:00Z",
+				"raw_message":       "{\"state\": \"DELETED\", \"org_guid\": \"53d03a41-16a7-40a3-b1a9-83298e65f8f5\", \"space_guid\": \"1cfdea3c-af34-4950-8383-5492e7c86c36\", \"space_name\": \"test-4-SPACE-dc424ba9f99abbf5\", \"service_guid\": \"7ab41178-f5a2-4444-b2f6-71abf43708b2\", \"service_label\": \"postgres\", \"service_plan_guid\": \"5eff64fa-8b41-4e6b-9321-b81624c678ce\", \"service_plan_name\": \"tiny-unencrypted-12\", \"service_broker_guid\": \"5cb906f6-ac20-4393-9210-e783fb4c8f39\", \"service_broker_name\": \"rds-broker\", \"service_instance_guid\": \"c64e2c78-db40-44ae-987c-f2171fe5e42d\", \"service_instance_name\": \"BACC-4-test-db-74b2529697d9ed58\", \"service_instance_type\": \"managed_service_instance\"}",
+			})).To(Succeed())
+
+    Expect(db.Query(`select * from update_resources('1970-01-01T00:00:00Z')`)).To(MatchJSON(testenv.Rows{
+        {
+        "num_rows_added": 1,
+        },
+      }))
+		Expect(
+			db.Query(`select valid_from,valid_to,resource_guid,resource_name,resource_type,org_guid,org_name,space_guid,space_name,plan_name,plan_guid,storage_in_mb,memory_in_mb,number_of_nodes,cf_event_guid from resources`),
+		).To(MatchJSON(testenv.Rows{
+			{
+         "cf_event_guid": "b6253aa7-ce44-4a2a-a9c2-f26a8c3b2c91",
+         "memory_in_mb": nil,
+         "number_of_nodes": nil,
+         "org_guid": "53d03a41-16a7-40a3-b1a9-83298e65f8f5",
+         "org_name": "53d03a41-16a7-40a3-b1a9-83298e65f8f5", // This cannot be consistently looked up for deleted test orgs
+         "plan_guid": "d5091c33-2f9d-4b15-82dc-4ad69717fc03",
+         "plan_name": "tiny-unencrypted-12",
+         "resource_guid": "c64e2c78-db40-44ae-987c-f2171fe5e42d",
+         "resource_name": "BACC-4-test-db-74b2529697d9ed58",
+         "resource_type": "service",
+         "space_guid": "1cfdea3c-af34-4950-8383-5492e7c86c36",
+         "space_name": "1cfdea3c-af34-4950-8383-5492e7c86c36",
+         "storage_in_mb": nil,
+         "valid_from": "2021-07-01T00:00:00+00:00",
+         "valid_to": "2021-07-15T00:00:00+00:00",
+			},
 		}))
 	})
 })
