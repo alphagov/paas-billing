9b66e55db85fdf6f41832faca2adb3244964d4e7
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2022-04-07 16:43:05 +0100

    Remove database objects for new billing
    As we will not be working on it for a while and it can cause confusion
    while debugging so we are  removing the billing db.

--- billing-db/tables/billing_formulae.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS billing_formulae;
-CREATE TABLE billing_formulae
-(
-  formula_name TEXT NOT NULL,
-  generic_formula TEXT NOT NULL,
-  formula_source TEXT NULL, -- Web page in AWS and/or other information showing where this formula came from
-
-  PRIMARY KEY (formula_name)
-);
