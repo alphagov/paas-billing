dbac8b592927e5431f2ffb3cdeb79551f16bd148
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2022-11-18 16:52:06 +0000

    TMP

--- eventstore/migrations/0120_something.sql
+++ eventstore/migrations/0120_something.sql
@@ -2,16 +2,83 @@
 
 BEGIN;
 
---
--- defining these parts of the main create_events query as functions should
--- allow us to ensure we're indexing and querying on the same expressions,
--- and if we're careful postgres is able to inline them into the query.
---
-
 
 CREATE FUNCTION app_event_resource_state(raw_message jsonb) returns resource_state AS $$
 	SELECT (raw_message->>'state')::resource_state;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
+
+CREATE FUNCTION app_event_filter(raw_message jsonb) returns BOOLEAN AS $$
+	SELECT (raw_message->>'state' = 'STARTED' or raw_message->>'state' = 'STOPPED')
+		and raw_message->>'space_name' !~ '^(SMOKE|ACC|CATS|PERF)-'; -- FIXME: this is open to abuse;
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
+
+CREATE FUNCTION app_resource_name(raw_message jsonb) returns text AS $$
+	SELECT raw_message->>'app_name';
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
+
+CREATE VIEW app_event_ranges AS SELECT
+		id as event_sequence,
+		guid::uuid as event_guid,
+		'app' as event_type,
+		created_at,
+		(raw_message->>'app_guid')::uuid as resource_guid,
+		coalesce(
+			app_resource_name(raw_message),
+			(array_remove(
+				array_agg(app_resource_name(raw_message)) over prev_events
+			, NULL))[1]
+		) as resource_name,
+		'app'::text as resource_type,                              -- resource_type for compute resources
+		(raw_message->>'org_guid')::uuid as org_guid,
+		(raw_message->>'space_guid')::uuid as space_guid,
+		'f4d4b95a-f55e-4593-8d54-3364c25798c4'::uuid as plan_guid, -- plan guid for all compute resources
+		'app'::text as plan_name,                                  -- plan name for all compute resources
+		'4f6f0a18-cdd4-4e51-8b6b-dc39b696e61b'::uuid as service_guid,
+		'app'::text as service_name,
+		coalesce(raw_message->>'instance_count', '1')::numeric as number_of_nodes,
+		coalesce(raw_message->>'memory_in_mb_per_instance', '0')::numeric as memory_in_mb,
+		'0'::numeric as storage_in_mb,
+		app_event_resource_state(raw_message) as state,
+		tstzrange(created_at,
+			lag(created_at, 1,
+				case when event_type = 'staging' then created_at
+				else now() end
+			) over resource_states
+		) as duration
+	FROM
+		app_usage_events
+	WHERE
+		app_event_filter(raw_message)
+	WINDOW
+		prev_events as (
+			partition by resource_guid
+			order by created_at desc, event_sequence desc
+			rows between current row and unbounded following
+		),
+		resource_states as (
+			partition by resource_guid
+			order by created_at desc, event_sequence desc
+			rows between current row and 1 preceding
+		);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 CREATE FUNCTION service_event_resource_state(raw_message jsonb) returns resource_state AS $$
 	SELECT (case
@@ -34,12 +101,6 @@ CREATE FUNCTION staging_event_resource_state(raw_message jsonb) returns resource
 		when (raw_message->>'state') = 'DELETED' then 'STOPPED'
 		when (raw_message->>'state') = 'UPDATED' then 'STARTED'
 	end)::resource_state;
-$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
-
-
-CREATE FUNCTION app_event_filter(raw_message jsonb) returns BOOLEAN AS $$
-	SELECT (raw_message->>'state' = 'STARTED' or raw_message->>'state' = 'STOPPED')
-		and raw_message->>'space_name' !~ '^(SMOKE|ACC|CATS|PERF)-'; -- FIXME: this is open to abuse;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 
 CREATE FUNCTION service_event_filter(raw_message jsonb) returns BOOLEAN AS $$
