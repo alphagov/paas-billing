0c279c388f97d8a771368af11dc356cb02b059b2
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2022-11-23 17:11:29 +0000

    TMP

---
+++ eventstore/migrations/0120_custom_aggregation.up.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION propagate_nonnull_agg_sfunc (state anyelement, newval anyelement) RETURNS anyelement AS $$
+	RETURN SELECT COALESCE(newval, state);
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
+
+CREATE AGGREGATE propagate_nonnull_agg (ORDER BY anyelement) (
+	SFUNC = propagate_nonnull_agg_sfunc
+	, STYPE = anyelement
+);
