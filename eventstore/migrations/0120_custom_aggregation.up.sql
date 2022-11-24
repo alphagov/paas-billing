c7cd44df47fc87d208edafff82c99dc86b0a3dd1
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2022-11-24 10:47:43 +0000

    TMP doesn't need to be ordered-set

--- eventstore/migrations/0120_custom_aggregation.up.sql
+++ eventstore/migrations/0120_custom_aggregation.up.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION propagate_nonnull_agg_sfunc (state anyelement, newval anyelement
 	SELECT COALESCE(newval, state);
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 
-CREATE AGGREGATE propagate_nonnull_agg (ORDER BY anyelement) (
+CREATE AGGREGATE propagate_nonnull_agg (anyelement) (
 	SFUNC = propagate_nonnull_agg_sfunc
 	, STYPE = anyelement
 );
