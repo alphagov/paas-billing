94d05a313b1ebb163aede786d2ce85652f1488e8
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2023-06-19 12:33:31 +0100

    BillableEventsHandler: don't use multiple db sessions
    the attempt to save app memory by splitting the query across
    multiple database sessions is done at the expense of database
    memory and connections, which is fundamentally a more expensive
    resource - currently, requests for big time ranges can easily
    exhaust the database's available connections (even if there
    isn't much data in the database).
    
    remove the use of multiple connections, but keep the month-
    splitting code as it allows us to take advantage of consolidated
    blocks (which are significantly faster to retrieve).
    
    fundamentally we need to either put result count limits in
    (perhaps a slightly awkward thing to introduce into an existing
    API in a backwards-compatible fashion) or perhaps investigate
    enforcing use of compressed connections (the use of "streaming"
    responses is also a bit tricky because it breaks proper error
    handling)

--- apiserver/server_billable_handler.go
+++ apiserver/server_billable_handler.go
@@ -2,12 +2,7 @@ package apiserver
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-
-	"io"
-
-	"strings"
 
 	"github.com/alphagov/paas-billing/apiserver/auth"
 	"github.com/alphagov/paas-billing/eventio"
@@ -16,6 +11,18 @@ import (
 
 func BillableEventsHandler(store eventio.BillableEventReader, consolidatedStore eventio.ConsolidatedBillableEventReader, uaa auth.Authenticator) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		sentOKHeader := false
+		sendOKHeader := func() error {
+			c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+			c.Response().WriteHeader(http.StatusOK)
+			if _, err := c.Response().Write([]byte("[\n")); err != nil {
+				return err
+			}
+			c.Response().Flush()
+			sentOKHeader = true
+			return nil
+		}
+
 		requestedOrgs := c.Request().URL.Query()["org_guid"]
 		if ok, err := authorize(c, uaa, requestedOrgs); err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, err)
@@ -35,118 +42,66 @@ func BillableEventsHandler(store eventio.BillableEventReader, consolidatedStore 
 		storeCtx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
-		// query the store
-		rowOfRows := RowOfRows{}
-		defer rowOfRows.Close()
-
 		months, err := filter.SplitByMonth()
 		if err != nil {
 			return err
 		}
+
+		delim := ""
 		for _, monthFilter := range months {
-			isConsolidated, err := consolidatedStore.IsRangeConsolidated(monthFilter)
+			err := func () error {  // so we can use defer in-loop
+				isConsolidated, err := consolidatedStore.IsRangeConsolidated(monthFilter)
+				if err != nil {
+					return err
+				}
+				var rows eventio.BillableEventRows
+				if isConsolidated {
+					rows, err = consolidatedStore.GetConsolidatedBillableEventRows(storeCtx, monthFilter)
+				} else {
+					rows, err = store.GetBillableEventRows(storeCtx, monthFilter)
+				}
+				if err != nil {
+					return err
+				}
+				defer rows.Close()
+
+				next := rows.Next()
+				for next {
+					b, err := rows.EventJSON()
+					if err != nil {
+						return err
+					}
+					if !sentOKHeader {
+						// this is sent as late as possible because any errors encountered after
+						// this won't be communicated to the client correctly
+						if err := sendOKHeader(); err != nil {
+							return err
+						}
+					}
+					if _, err := c.Response().Write([]byte(delim)); err != nil {
+						return err
+					}
+					if _, err := c.Response().Write(b); err != nil {
+						return err
+					}
+					delim = ",\n"
+					next = rows.Next()
+					c.Response().Flush()
+				}
+				return nil
+			}()
 			if err != nil {
 				return err
 			}
-			var rows eventio.BillableEventRows
-			if isConsolidated {
-				rows, err = consolidatedStore.GetConsolidatedBillableEventRows(storeCtx, monthFilter)
-			} else {
-				rows, err = store.GetBillableEventRows(storeCtx, monthFilter)
-			}
-			if err != nil {
+		}
+		if !sentOKHeader {
+			if err := sendOKHeader(); err != nil {
 				return err
 			}
-			rowOfRows.RowsCollection = append(rowOfRows.RowsCollection, rows)
 		}
-
-		// stream response to client
-		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
-		c.Response().WriteHeader(http.StatusOK)
-		return WriteRowsAsJson(c.Response(), c.Response(), &rowOfRows)
-	}
-}
-
-func WriteRowsAsJson(writer io.Writer, flusher http.Flusher, rows eventio.BillableEventRows) error {
-	if _, err := writer.Write([]byte("[\n")); err != nil {
-		return err
-	}
-	flusher.Flush()
-	next := rows.Next()
-	delim := "\n"
-	for next {
-		b, err := rows.EventJSON()
-		if err != nil {
+		if _, err := c.Response().Write([]byte("\n]\n")); err != nil {
 			return err
 		}
-		if _, err := writer.Write(b); err != nil {
-			return err
-		}
-		next = rows.Next()
-		if next {
-			delim = ",\n"
-		} else {
-			delim = "\n"
-		}
-		if _, err := writer.Write([]byte(delim)); err != nil {
-			return err
-		}
-		flusher.Flush()
-	}
-	if _, err := writer.Write([]byte("]\n")); err != nil {
-		return err
-	}
-	flusher.Flush()
-	return rows.Err()
-}
-
-type RowOfRows struct {
-	RowsCollection []eventio.BillableEventRows
-	index          int
-}
-
-func (r *RowOfRows) Next() bool {
-	if r.index >= len(r.RowsCollection) {
-		return false
-	}
-	if r.RowsCollection[r.index].Next() {
-		return r.Err() == nil
-	}
-	r.index = r.index + 1
-	return r.Next()
-}
-
-func (r *RowOfRows) Close() error {
-	errs := []string{}
-	for _, r := range r.RowsCollection {
-		err := r.Close()
-		if err != nil {
-			errs = append(errs, err.Error())
-		}
-	}
-	if len(errs) == 0 {
 		return nil
 	}
-	return fmt.Errorf("errors happened calling RowOfRows.Close(): %s", strings.Join(errs, " | "))
-}
-
-func (r *RowOfRows) Err() error {
-	if r.index >= len(r.RowsCollection) {
-		return nil
-	}
-	return r.RowsCollection[r.index].Err()
-}
-
-func (r *RowOfRows) EventJSON() ([]byte, error) {
-	if r.index >= len(r.RowsCollection) {
-		return nil, fmt.Errorf("no more data in RowsCollection")
-	}
-	return r.RowsCollection[r.index].EventJSON()
-}
-
-func (r *RowOfRows) Event() (*eventio.BillableEvent, error) {
-	if r.index >= len(r.RowsCollection) {
-		return nil, fmt.Errorf("no more data in RowsCollection")
-	}
-	return r.RowsCollection[r.index].Event()
 }
